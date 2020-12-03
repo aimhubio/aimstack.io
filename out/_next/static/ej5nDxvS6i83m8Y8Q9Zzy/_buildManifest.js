7f36ebc06c8cb17c4cdb8a5fab86ababb1b062b7
Ruben Karapetyan <karapetyanrouben@gmail.com>
2020-12-03 23:44:14 +0400

    Enable out folder for deployment

---
+++ out/_next/static/ej5nDxvS6i83m8Y8Q9Zzy/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F6540b9fff9997f929a5c.css","static\u002Fchunks\u002Fpages\u002Findex-6bb16b385e0c0926a4f3.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-903167cea88af930f80c.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
