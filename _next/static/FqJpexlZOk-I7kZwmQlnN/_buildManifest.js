e123e00b5f8213a3f87a067cf09ff7059c5cd129
SGevorg <gevorg.soghomonyan.gs@gmail.com>
2020-12-08 09:52:01 +0400

    Modify the content and minor changes to the structure

--- _next/static/FqJpexlZOk-I7kZwmQlnN/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fc66e4d78492618403182.css","static\u002Fchunks\u002Fpages\u002Findex-0e29a6cb5198cd4dcf62.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-903167cea88af930f80c.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
