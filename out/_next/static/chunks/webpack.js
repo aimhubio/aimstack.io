b48cab644b56fb68ee8d24e8a9747f949e8d58f8
Gor Arakelyan <arakelyangor10@gmail.com>
2020-12-16 16:18:31 +0400

    Build the new version

--- out/_next/static/chunks/webpack.js
+++ out/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "07928753f38ff6d2d803";
+/******/ 	var hotCurrentHash = "32880b25c18750d539ab";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
