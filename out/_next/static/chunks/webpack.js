dc101ef5d4b2418fcf3f85255d7d3db08beea634
Gor Arakelyan <arakelyangor10@gmail.com>
2021-01-05 17:34:25 +0400

    Update slack invite link

--- out/_next/static/chunks/webpack.js
+++ out/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "32880b25c18750d539ab";
+/******/ 	var hotCurrentHash = "995ae35389b91e01b860";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
