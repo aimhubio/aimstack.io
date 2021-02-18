8b0d6afa5c1620f2df573d51a77c815f8013acb9
Gor Arakelyan <arakelyangor10@gmail.com>
2021-02-18 18:54:51 +0400

    New website layout (#7)

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
+import '../styles/theme.css';
 import '../styles/globals.css';
+import '../styles/icons.css';
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
