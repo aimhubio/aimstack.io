95e52c76f104019c84abdfaf981ad90163667137
Gor Arakelyan <arakelyangor10@gmail.com>
2021-03-10 02:57:02 +0400

    Update captcha

--- out/_next/static/n_1Prma-LDFR5UfbeA3TL/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F49d3049c49a33845e7fb.css","static\u002Fchunks\u002Fpages\u002Findex-a9a01d664fb5c81c4a59.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-9cf354afed5fb1ed616f.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
