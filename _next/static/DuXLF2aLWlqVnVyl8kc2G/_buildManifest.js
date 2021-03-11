5579573f92626a3a569a801c746cfd07f343a1a7
Gor Arakelyan <arakelyangor10@gmail.com>
2021-03-11 16:43:51 +0400

    Add learn page

--- _next/static/DuXLF2aLWlqVnVyl8kc2G/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F97872ff5ec6a9764b089.css","static\u002Fchunks\u002Fpages\u002Findex-dcb749bc78acdfbb4d8e.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-9cf354afed5fb1ed616f.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
