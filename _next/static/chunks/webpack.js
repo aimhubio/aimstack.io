789cd1fd13d8e70f51dbb41396e7d6bca2983f29
Gor Arakelyan <arakelyangor10@gmail.com>
2021-03-15 16:05:34 +0400

    Add latest tutorial

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1f929d60862f9bb9f4ea";
+/******/ 	var hotCurrentHash = "e91d46ee1ad0aafefeb0";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
