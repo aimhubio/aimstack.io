1d4570392e6a7b3a1ebe4a182e1942293902bed6
Gor Arakelyan <arakelyangor10@gmail.com>
2023-01-16 16:22:49 +0400

    [fix] Use tmp domain v4.aimstack.io

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const SITE_URL = 'https://aimstack.io/';
+const SITE_URL = 'https://v4.aimstack.io/';
 export const GITHUB_API = 'https://api.github.com/repos/aimhubio/aim/';
 // const SITE_URL= 'http://localhost:3000'
 // const SITE_URL= process.env.SITE_URL  // it not work
