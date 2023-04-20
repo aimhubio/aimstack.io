954a9bd43ad0b19b9adbc7e39ed2f31fb1936be1
Arsen Alkamyan <alkamyan97@gmail.com>
2023-04-20 18:03:04 +0400

    [fix] Fix unnecessary pages rebuilding

--- next.config.js
+++ next.config.js
@@ -1,34 +1,36 @@
 const { withContentlayer } = require('next-contentlayer');
-const withExportImages = require('next-export-optimize-images')
+const withExportImages = require('next-export-optimize-images');
+const path = require('path');
 
-const nextConfig = withExportImages({
-  async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "cache-control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-    ];
-  },
-  reactStrictMode: true,
+module.exports = withContentlayer(
+  withExportImages({
+    async headers() {
+      return [
+        {
+          source: '/:path*',
+          headers: [
+            {
+              key: 'cache-control',
+              value: 'public, max-age=31536000, immutable',
+            },
+          ],
+        },
+      ];
+    },
+    reactStrictMode: true,
+    disableImportAliasWarning: true,
+    future: {
+      webpack5: true,
+      incremental: true,
+    },
+    webpack: (config, { isServer }) => {
+      // Enable only in Production
+      if (!isServer && process.env.NODE_ENV === 'production') {
+        config.optimization.moduleIds = 'size';
+        config.optimization.chunkIds = 'size';
+      }
 
-  // optimizeFonts: false,
-  // images: {
-  //   // remotePatterns: [
-  //   //   {
-  //   //     protocol: 'https',
-  //   //     hostname: 'aim-netlify-image.s3.amazonaws.com',
-  //   //     port: '',
-  //   //     // pathname: '/account123/**',
-  //   //   },
-  //   // ],
-  // },
-  disableImportAliasWarning: true,
-});
-
-module.exports = withContentlayer(nextConfig);
+      return config;
+    },
+  })
+);
