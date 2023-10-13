5a18f9e273b64ca795b6daf92fc766bcf5af4b3b
Karo <44981312+KaroMourad@users.noreply.github.com>
2023-10-14 01:14:36 +0400

    [fix] Fix adding metadata of aimos page

--- content/SeoData/aimos.ts
+++ content/SeoData/aimos.ts
@@ -3,7 +3,7 @@ const aimOsSeo = {
   description:
     "Open-source modular observability for AI Systems. Easily log, connect and observe any parts of your AI Systems from experiments to production to prompts to ai system monitoring",
   image: '',
-  path: '',
+  path: 'aimos',
 };
 
 export default aimOsSeo;
