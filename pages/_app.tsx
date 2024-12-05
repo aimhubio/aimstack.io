9fac83057da1780120415ba23ce9e24d38850766
mihran113 <vanyanmihran@gmail.com>
2024-12-05 18:49:50 +0400

    Use correct script for ts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,15 +5,17 @@ import { useEffect } from 'react';
 function App({ Component, pageProps }) {
   useEffect(() => {
     // Directly adding your script content here
-    !function () {
-      let reb2b = window.reb2b = window.reb2b || [];
+    (function () {
+      let reb2b = window.reb2b || [];
+      window.reb2b = reb2b;
       if (reb2b.invoked) return;
       reb2b.invoked = true;
       reb2b.methods = ["identify", "collect"];
-      reb2b.factory = function (method) {
+      reb2b.factory = function (method: string) {
         return function () {
-          var args = Array.prototype.slice.call(arguments);
-          args.unshift(method);reb2b.push(args);
+          let args = Array.prototype.slice.call(arguments);
+          args.unshift(method);
+          reb2b.push(args);
           return reb2b;
         };
       };
@@ -21,17 +23,19 @@ function App({ Component, pageProps }) {
         let key = reb2b.methods[i];
         reb2b[key] = reb2b.factory(key);
       }
-      reb2b.load = function (key) {
+      reb2b.load = function (key: string) {
         let script = document.createElement("script");
         script.type = "text/javascript";
         script.async = true;
         script.src = "https://s3-us-west-2.amazonaws.com/b2bjsstore/b/" + key + "/reb2b.js.gz";
         let first = document.getElementsByTagName("script")[0];
-        first.parentNode.insertBefore(script, first);
+        if (first && first.parentNode) {
+          first.parentNode.insertBefore(script, first);
+        }
       };
       reb2b.SNIPPET_VERSION = "1.0.1";
       reb2b.load("4N210HE2MG6Z");
-    }();
+    })();
   }, []);
 
   return (
